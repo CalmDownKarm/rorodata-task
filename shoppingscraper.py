648e22b7953c0eb55a2e693a81f628c173ef2e0b
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2017-11-16 17:14:27 +0530

    Just need to failure test now

--- shoppingscraper.py
+++ shoppingscraper.py
@@ -1,94 +1,118 @@
+'''Simple Web Scraper for Shopping.com products, requires
+Requests, Click, Json and beautifulsoup'''
+
+import json
 import requests
 import click
 from bs4 import BeautifulSoup as bs
 
 def make_request(url):
     '''Makes a request to the passed url, returns a beautifulsoup object'''
-    headers={'User-Agent':
-     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36'} 
-    requestpage = requests.get(url,headers = headers, timeout=15)
+    headers = {'User-Agent':
+               'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36'}
+    try:
+        requestpage = requests.get(url, headers=headers, timeout=15)
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return
     if requestpage.status_code == 200:
-        soup = bs(requestpage.text,'lxml')
+        soup = bs(requestpage.text, 'lxml')
     return soup
+
 
 def return_num_items(url):
     '''Helper, Returns the number of items returned for a particular keyword
     http://www.shopping.com/products?KW=<keyword>'''
     try:
         soup = make_request(url)
-        div = soup.find('div', {'id': "sortFiltersBox"}).find("span").get("name")
+        if not soup:
+            return
+        div = soup.find('div', {'id': "sortFiltersBox"}
+                        ).find("span").get("name")
         num_items = int(div.split(':')[1])
-        print(num_items)
+        print(repr(num_items)+" Items")
     except AttributeError:
-        #Call Logging function
+        # Call Logging function
         print("\n Attribute Error")
     except IndexError:
         print("\n Index Error")
     return num_items
 
+
 def build_dict(product):
+    '''Builds a dictionary given an html div as a soup object'''
     temp = {}
     # Not Grabbing URLs to product images
-    product_name = product.find("a", {"class":"productName"})
+    product_name = product.find("a", {"class": "productName"})
     if product_name:
         temp["Product Name"] = product_name.get("title")
         if not temp['Product Name']:
-            temp["Product Name"]=product_name.find("span").get("title")
-    product_price = product.find("span", {"class":"productPrice"})
+            temp["Product Name"] = product_name.find("span").get("title")
+    product_price = product.find("span", {"class": "productPrice"})
     if product_price:
         temp["Product Price"] = product_price.get_text().strip()
-    product_merchant = product.find("span", {"class":"newMerchantName"})
-    
+    product_merchant = product.find("span", {"class": "newMerchantName"})
+
     if not product_merchant:
-        product_merchant = product.find("a",{"class":"newMerchantName"})
+        product_merchant = product.find("a", {"class": "newMerchantName"})
     temp["Product Merchant"] = product_merchant.get_text().strip()
-    
-    product_url = product.find("div", {"class":["productGrid","gridItemTop"]})
+
+    product_url = product.find(
+        "div", {"class": ["productGrid", "gridItemTop"]})
     if product_url:
         p_url = product_url.find("a")
         if p_url:
-            temp["Product URL"] = "http://www.shopping.com"+p_url.get("href")
+            temp["Product URL"] = "http://www.shopping.com" + p_url.get("href")
 
-    product_shipping = product.find("div",{"class":["taxShippingArea","freeShip"]})
-    
+    product_shipping = product.find(
+        "div", {"class": ["taxShippingArea", "freeShip"]})
+
     if product_shipping:
         temp["Product Shipping"] = product_shipping.get_text().strip()
-    
+
     return temp
+
 
 def return_items(url):
     '''Helper, Returns a JSON file of the products returned on a particular
     page for a particular keyword'''
     soup = make_request(url)
+    if not soup:
+        return
     productdivs = soup.find_all("div", {"class": "gridBox"})
     list_of_products = [build_dict(product) for product in productdivs]
+    print(repr(len(list_of_products))+" Items returned")
     return list_of_products
 
+
 @click.command()
-@click.argument('keyw', type=click.STRING, nargs =-1)
+@click.argument('keyw', type=click.STRING, nargs=-1)
 @click.option('--pg', type=click.STRING, default=None)
 def entrypoint(keyw=None, pg=None):
-    '''Entry Point for the program, makes request and calls respective helpers for query'''   
+    '''Entry Point for the program, makes request and calls respective helpers for query'''
     if not keyw:
         print("No keyword supplied")
         return
     else:
+        # Multi Keyword Argument comes as a tuple, need to cast to string
         keyw = ' '.join(keyw)
     if pg:
         # Query 2 URL
         # Create different url
         url = "http://www.shopping.com/products~PG-<number>?KW="
-        url = url.replace('<number>', pg)+keyw
-        
-        import pandas
-        df = pandas.DataFrame(return_items(url))
-        df.to_csv("Products.csv")
+        url = url.replace('<number>', pg) + keyw
+        listofproducts = return_items(url)
+        if not listofproducts:
+            print("No Items Returned, please check Keywords/Page Numbers")
+        else:
+            with open("products.json", mode='w') as fileout:
+                json.dump(listofproducts, fileout)
 
     else:
-        #Query 1 URL
-        url = 'http://www.shopping.com/products?sb=1&KW='+ keyw
+        # Query 1 URL
+        url = 'http://www.shopping.com/products?sb=1&KW=' + keyw
         return_num_items(url)
-        
+
 
 if __name__ == "__main__":
     entrypoint()
